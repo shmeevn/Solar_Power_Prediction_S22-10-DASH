45bbc0a42058bdd257898fdc18a69871719bd3ba
WPhoenix1999 <87554813+WPhoenix1999@users.noreply.github.com>
2022-10-25 12:27:35 -0500

    Add files via upload

---
+++ Fall/Auto-Updating HTML with Keras/figs/LSTM.py
@@ -0,0 +1,53 @@
+#For validation test:
+import numpy as np
+from sklearn.metrics import mean_squared_error
+
+#For the LSTM:
+import keras
+from keras.layers import Input, Dense, LSTM, Dropout
+from keras.models import Sequential
+from keras.callbacks import EarlyStopping
+
+#This class is used so that the per-step training losses can be recorded for parity with pyTorch
+class batch_loss_Callback(keras.callbacks.Callback):
+    def __init__(self, metric):
+        self.losses = []
+        self.metric = metric
+    def on_batch_end(self, batch, logs=None):
+        self.losses.append(logs[self.metric])
+
+#This function instatiates the Keras LSTM model, using Keras Sequential
+def makeModel(inputSize, seq_length, batchSize, recurrentUnits, outputSize, drop = 0.0, lossf = 'mse'):
+    model = Sequential()#Keras has 3 methods of implementing models, Sequential, Functional, and subclassing
+    #Sequential is used here as it was used in the kaggle sample, but the Functional API is fully-featured (MIMO architectures, complicated models)
+    #The architecture used here was chosen to be 'comparable' to the PyTorch architecture, consisting of a reccurent layer, dropout, and a dense (Linear) output.
+    model.add(Input(shape=(seq_length, inputSize), batch_size = batchSize))#The input layers defines the input shape
+    model.add(LSTM(recurrentUnits, return_sequences=True))
+    model.add(Dropout(drop))
+    model.add(Dense(outputSize))
+    #The adam optimizer is also used in the PyTorch model, Keras offers SGD, RMSprop, Adadelta, and some others in addition to adam
+    #The optimizer can be configured by instantiating an optimizer object and passing parameters to its constructor,
+    #   that object is then passed to the 'optimizer' argment of the compile() function below. Passing a string is a shortcut to using default parameters.
+    model.compile(loss=lossf, optimizer='adam')
+    return model
+
+#This function trains the model
+def trainModel(model, train_x, train_y, test_x, test_y, numEpochs, batchSize):
+    #This is the list of callbacks to call during training
+    cbl = [batch_loss_Callback('loss'), EarlyStopping(monitor='val_loss', min_delta=0.01, patience=3, restore_best_weights=True)]
+    #These give the indices to truncate the train/test sets to avoid an error
+    lim1 = (len(train_x)//batchSize)*batchSize
+    lim2 = (len(test_x)//batchSize)*batchSize
+    #This call trains the model
+    history = model.fit(train_x[:lim1], train_y[:lim1], epochs=numEpochs, batch_size=batchSize, validation_data=(test_x[:lim2], test_y[:lim2]), verbose=2, shuffle=False, callbacks=cbl)
+    return model, history, cbl[0].losses
+
+#This function tests the model
+def ValidationTest(model, test_x, test_y, batch_size):
+    lim = (len(test_x)//batch_size)*batch_size
+    pred = model.predict(test_x[:lim], batch_size)
+    rmse = [ mean_squared_error(test_y[i], pred[i]) for i in range(len(pred))]
+    return rmse, np.reshape(test_y[:lim], (-1)), np.reshape(pred, (-1))
+    
+def Predict(model, x, batch_size):
+    return model.predict(x, batch_size)
---
+++ Fall/Auto-Updating HTML with Keras/figs/testbench.py
@@ -0,0 +1,98 @@
+import LSTM as lstm
+import tb_util as tbu
+import matplotlib.pyplot as plt #for plotting
+import time
+import numpy as np
+import influxdb_client
+from influxdb_client.client.write_api import SYNCHRONOUS
+import pdb   
+
+##Parameters
+input_size = 2              #Number of input features
+output_size = 1             #Number of output features
+recurrent_units = 32        #Number of RNN/LSTM cells
+seq_length = 5              #Number of input-vectors per sample
+numEpochs = 200              #Number of training passes
+batch_size = 1              #How many samples should be ran in parallel per training step
+drop = 0.0                  #Dropout parameter
+train_test_split = 0.8      #Training/Validation split percentage as decimal, 0.8 -> 80% training, 20% Validation
+loss_function = 'mse'       #Loss function to use with the optimizer, 'mse' and 'mae' should work
+n = 1
+t = 0
+
+#Load Dataset, specify columns by name, None means all
+inputColumns = ['Time', 'PV_Power']
+outputColumns = ['PV_Power']
+Xt, Yt, Xv, Yv, X, Y = tbu.loadData("Formatted_PV_data.csv", seq_length, train_test_split, inputColumns, outputColumns)
+
+#for the LSTM
+model = lstm.makeModel(input_size, seq_length, batch_size, recurrent_units, output_size, drop, loss_function)
+model, khistory, trainHistory2 = lstm.trainModel(model, Xt, Yt, Xv, Yv, numEpochs, batch_size)
+
+plt.rcParams["figure.figsize"] = [7.50, 3.50]
+plt.rcParams["figure.autolayout"] = True
+plt.style.use('dark_background')
+
+yy1 = []
+yy2 = []
+xx = []
+
+while True:
+    try:
+        pred = lstm.Predict(model, X[n:n+1], batch_size)
+        if n + 1 <= X.size:
+            n = n + 1
+        else:
+            n = 1
+            
+        plt.clf()
+        
+        yy1.append(float(pred[0][0]))
+        yy2.append(float(Y[n-1:n][0][0]))
+        xx.append(t)
+        
+        yy1.append(float(pred[0][1]))
+        yy2.append(float(Y[n-1:n][0][1]))
+        xx.append(t+1)
+        
+        yy1.append(float(pred[0][2]))
+        yy2.append(float(Y[n-1:n][0][2]))
+        xx.append(t+2)
+        
+        yy1.append(float(pred[0][3]))
+        yy2.append(float(Y[n-1:n][0][3]))
+        xx.append(t+3)
+        
+        yy1.append(float(pred[0][4]))
+        yy2.append(float(Y[n-1:n][0][4]))
+        xx.append(t+4)
+        
+        if len(yy1) > 100:
+            yy1 = yy1[5:]
+            yy2 = yy2[5:]
+            xx = xx[5:]
+        
+        plt.plot(xx, yy1, 'r-', label="Prediction")
+        plt.plot(xx, yy2, 'g-', label="True")
+        plt.legend()
+        
+        #plt.scatter(t, float(pred[0][0]))
+        #plt.plot(t, float(Y[n-1:n][0][0]), 'g-', label="True Value")
+        
+        #plt.scatter(t+1, float(pred[0][1]))
+        #plt.plot(t+1, float(Y[n-1:n][0][1]), 'g-', label="True Value")
+        
+        #plt.scatter(t+2, float(pred[0][2]))
+        #plt.plot(t+2, float(Y[n-1:n][0][2]), 'g-', label="True Value")
+        
+        #plt.scatter(t+3, float(pred[0][3]))
+        #plt.plot(t+3, float(Y[n-1:n][0][3]), 'g-', label="True Value")
+        
+        #plt.scatter(t+4, float(pred[0][4]))
+        #plt.plot(t+4, float(Y[n-1:n][0][4]), 'g-', label="True Value")
+        
+        time.sleep(1)
+        plt.savefig("fig.png")
+        t = t + 5
+    except Exception:
+        print("Error")
